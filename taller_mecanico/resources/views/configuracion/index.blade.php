faa9f5b00ed27d44f050fa3fbe78991c48eacb27
YEFER779 <alexanderumanzor243@gmail.com>
2025-11-28 22:54:24 +0000

    Manuales Adm,Empleado,Cliente descargables

--- taller_mecanico/resources/views/configuracion/index.blade.php
+++ taller_mecanico/resources/views/configuracion/index.blade.php
@@ -4,10 +4,30 @@
 <div style="padding: 20px;">
     <h2 style="font-size: 22px; margin-bottom: 20px;">⚙️ Configuración del Sistema</h2>
 
-    <a href="{{ route('configuracion.create') }}" 
-       style="display: inline-block; padding: 8px 12px; color: white; background: #222; border-radius: 5px; text-decoration: none; margin-bottom: 20px;">
-        ➕ Agregar Configuración
+    <a href="{{ route('configuracion.create') }}"
+        style="display: inline-block; padding: 8px 12px; color: white; background: #222; border-radius: 5px; text-decoration: none; margin-bottom: 20px;">
+        ➕ Lista de Recordatorios
     </a>
+
+    <a href="{{ asset('manuales/manual_administrador.pdf') }}"
+        download
+        style="display: inline-block; padding: 8px 12px; color: white; background: #0066cc; border-radius: 5px; text-decoration: none; margin-left: 10px;">
+        📘 Manual Administrador
+    </a>
+
+    <a href="{{ asset('manuales/manual_empleados.pdf') }}"
+        download
+        style="display: inline-block; padding: 8px 12px; color: white; background: #04ea04ff; border-radius: 5px; text-decoration: none; margin-left: 10px;">
+        📘 Manual Empleados
+    </a>
+
+    <a href="{{ asset('manuales/manual_clientes.pdf') }}"
+        download
+        style="display: inline-block; padding: 8px 12px; color: white; background: #d11622ff; border-radius: 5px; text-decoration: none; margin-left: 10px;">
+        📘 Manual Clientes
+    </a>
+
+
 
     <table style="width: 100%; border-collapse: collapse; margin-top: 20px; background: white;">
         <thead>
@@ -22,28 +42,28 @@
 
         <tbody>
             @foreach ($configuraciones as $config)
-                <tr>
-                    <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->config_id }}</td>
-                    <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->clave }}</td>
-                    <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->valor }}</td>
-                    <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->descripcion }}</td>
-                    <td style="padding: 10px; border: 1px solid #ccc;">
-                        <a href="{{ route('configuracion.edit', $config->config_id) }}" 
-                           style="padding: 5px 8px; background: #444; color: white; border-radius: 4px; text-decoration: none; margin-right:5px;">
-                            ✏️ Editar
-                        </a>
+            <tr>
+                <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->config_id }}</td>
+                <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->clave }}</td>
+                <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->valor }}</td>
+                <td style="padding: 10px; border: 1px solid #ccc;">{{ $config->descripcion }}</td>
+                <td style="padding: 10px; border: 1px solid #ccc;">
+                    <a href="{{ route('configuracion.edit', $config->config_id) }}"
+                        style="padding: 5px 8px; background: #444; color: white; border-radius: 4px; text-decoration: none; margin-right:5px;">
+                        ✏️ Editar
+                    </a>
 
-                        <form action="{{ route('configuracion.destroy', $config->config_id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit"
-                                style="padding: 5px 8px; background: #c0392b; color: white; border: none; border-radius: 4px; cursor: pointer;"
-                                onclick="return confirm('¿Estás seguro de eliminar esta configuración?')">
-                                🗑️ Eliminar
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+                    <form action="{{ route('configuracion.destroy', $config->config_id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"
+                            style="padding: 5px 8px; background: #c0392b; color: white; border: none; border-radius: 4px; cursor: pointer;"
+                            onclick="return confirm('¿Estás seguro de eliminar esta configuración?')">
+                            🗑️ Eliminar
+                        </button>
+                    </form>
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
